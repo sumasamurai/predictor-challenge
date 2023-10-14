908f89b200efd28cf20950c39586ae70b1e0d44d
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-10-14 11:26:19 -0700

    add basic slider skeleton

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,14 +1,14 @@
-import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
+import { Slider } from "~~/components/Slider";
 
 const Home: NextPage = () => {
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <h1 className="text-4xl font-bold my-10">Predictor Game</h1>
+      <div className="">
+        <h1 className="text-4xl text-center font-bold my-10">Predictor Game</h1>
+        <Slider />
       </div>
     </>
   );
