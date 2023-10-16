41b6c92959e9a00d84a661a777a739fe366e0072
Lavishq <lavishq47@gmail.com>
2023-10-16 10:01:29 +0530

    fixed the compilation error

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -5,12 +5,44 @@ const contracts = {
       name: "localhost",
       contracts: {
         PredictorGame: {
-          address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+          address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
           abi: [
             {
               inputs: [],
               stateMutability: "nonpayable",
               type: "constructor",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: true,
+                  internalType: "uint256",
+                  name: "epoch",
+                  type: "uint256",
+                },
+                {
+                  indexed: false,
+                  internalType: "int256",
+                  name: "price",
+                  type: "int256",
+                },
+              ],
+              name: "CloseRound",
+              type: "event",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: true,
+                  internalType: "uint256",
+                  name: "epoch",
+                  type: "uint256",
+                },
+              ],
+              name: "StartRound",
+              type: "event",
             },
             {
               inputs: [],
@@ -33,6 +65,47 @@ const contracts = {
                   internalType: "uint256[2]",
                   name: "",
                   type: "uint256[2]",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              name: "ledger",
+              outputs: [
+                {
+                  components: [
+                    {
+                      internalType: "uint256",
+                      name: "positionId",
+                      type: "uint256",
+                    },
+                  ],
+                  internalType: "struct PredictorGame.Position",
+                  name: "position",
+                  type: "tuple",
+                },
+                {
+                  internalType: "uint256",
+                  name: "amount",
+                  type: "uint256",
+                },
+                {
+                  internalType: "bool",
+                  name: "claimed",
+                  type: "bool",
                 },
               ],
               stateMutability: "view",
@@ -129,6 +202,30 @@ const contracts = {
               name: "setMinBet",
               outputs: [],
               stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              name: "userRounds",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
               type: "function",
             },
             {
