d2fd0eba2aa3b94b2b924f6d53ccbd30a0d6ac8d
“sumasamurai” <pushsumasamurai@gmail.com>
2023-10-22 10:39:46 +0300

    Add new logo url

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -81,7 +81,7 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/favicon.png" />
+            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.png" />
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight tracking-[0.05rem]">Predictor</span>
